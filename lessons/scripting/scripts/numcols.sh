1dab75fa69041546b9d0916a02897f525a1e54cc
Hilmar Lapp <hlapp@drycafe.net>
2014-05-18 23:41:30 -0400

    Adds hashbang shell lines, formatting, path variables.

--- lessons/scripting/scripts/numcols.sh
+++ lessons/scripting/scripts/numcols.sh
@@ -1,5 +1,6 @@
-for csvfile in $*
-do
-numcols=$(expr `head -n 1 $csvfile | tr -d -c , | wc -c` + 1)
-echo "$csvfile: $numcols columns"
+#!/bin/bash
+
+for csvfile in $* ; do
+    numcols=$(expr `head -n 1 $csvfile | tr -d -c , | wc -c` + 1)
+    echo "$csvfile: $numcols columns"
 done
--- lessons/scripting/scripts/workflow.sh
+++ lessons/scripting/scripts/workflow.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
+# paths to programs
+sqlite=/usr/local/opt/sqlite/bin/sqlite3
+R=R
+
 # Load the data into database, combine, filter, export
-cat sqlite-data-import.sql | /usr/local/opt/sqlite/bin/sqlite3 > result.csv
+cat sqlite-data-import.sql | $sqlite > result.csv
 
 # for old versions of sqlite
-# cat sqlite-data-import-from-db.sql | sqlite3 portal_mammals.sqlite
+# cat sqlite-data-import-from-db.sql | $sqlite portal_mammals.sqlite
 
 # Load into R for analysis and plotting
-R CMD BATCH barplot-figure.R
+$R CMD BATCH barplot-figure.R
