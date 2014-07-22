aab5c6e1a3f9cfc7e42e20cd74e2a6ba3bd7ca7f
Hilmar Lapp <hlapp@drycafe.net>
2014-07-22 15:00:10 -0400

    Fixes sqlite script file name and old version command.

--- lessons/scripting/scripts/workflow.sh
+++ lessons/scripting/scripts/workflow.sh
@@ -5,10 +5,10 @@ sqlite=/usr/local/opt/sqlite/bin/sqlite3
 R=R
 
 # Load the data into database, combine, filter, export
-cat sqlite-data-import.sql | $sqlite > result.csv
+cat sqlite-import.sql | $sqlite > result.csv
 
 # for old versions of sqlite
-# cat sqlite-data-import-from-db.sql | $sqlite portal_mammals.sqlite
+# cat sqlite-select-from-db.sql | $sqlite portal_mammals.sqlite > result.csv
 
 # Load into R for analysis and plotting
 $R CMD BATCH barplot-figure.R
